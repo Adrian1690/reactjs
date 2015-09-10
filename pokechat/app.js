0aad745b31bea7a0aedb25a7d4e24acaf893bca4
Adrian1690 <j.adrian.1690@gmail.com>
2015-09-10 16:19:51 -0500

    construyendo build

--- pokechat/app.js
+++ pokechat/app.js
@@ -3,13 +3,13 @@
  */ 
 
 import React from 'react';
-import PokeTable from '/components/PokeTable';
+import PokeTable from './components/PokeTable';
+
 var pokemos = [
 	{ number:1 , name:'Bulbasaur' , years: 15},
 	{ number:2 , name:'Ivysaur' , years: 20},
 	{ number:3 , name:'VenuSaur' , years: 25},
 ];
-var pokemon = pokemos[1];
 
 React.render(
 	<PokeTable pokemons={pokemos} />
